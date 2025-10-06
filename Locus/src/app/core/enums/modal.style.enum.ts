41891b704ccd5dcc630235033866c2c31e7ecc44
Maria Joaquina <mariaklmn@outlook.com>
2025-10-06 18:52:26 -0300

    Melhoria nos estilos do modal genérico

---
+++ Locus/src/app/core/enums/modal.style.enum.ts
@@ -0,0 +1,5 @@
+export enum ModalStyleClassEnum {
+    MEDIUM = "modal-medium",
+    SMALL = "modal-small",
+    LARGE = "modal-large"
+}
--- Locus/src/app/shared/components/generic-modal/generic-modal.component.ts
+++ Locus/src/app/shared/components/generic-modal/generic-modal.component.ts
@@ -3,6 +3,7 @@ import { Button } from 'primeng/button';
 import { NgIf } from '@angular/common';
 import { PrimeTemplate } from 'primeng/api';
 import { DialogModule } from 'primeng/dialog';
+import { ModalStyleClassEnum } from 'src/app/core/enums/modal.style.enum';
 
 @Component({
     selector: 'app-generic-modal',
@@ -13,16 +14,18 @@ import { DialogModule } from 'primeng/dialog';
 })
 export class GenericModalComponent implements OnInit {
 
-  @Input() visible: boolean = false;
-  @Input() header = '';
+  @Input({ required: true }) visible = false;
+  @Input({ required: true }) header? = '';
   @Input() style: any = null;
   @Input() showConfirmButton = true;
   @Input() showCancelButton = true;
   @Input() confirmLabel = 'Salvar';
   @Input() cancelLabel = 'Cancelar';
-  @Input() cancelButtonStyleClass = 'p-button-text text-danger';
+  @Input() cancelButtonStyleClass = 'p-button-text p-button-danger';
   @Input() confirmButtonStyleClass = 'p-button-primary';
-  @Input() onLoading: boolean = false;
+  @Input() isDelete = false;
+  @Input() onLoading = false;
+  @Input() modalStyle: ModalStyleClassEnum = ModalStyleClassEnum.MEDIUM;
 
   @Output() onClose = new EventEmitter();
   @Output() onConfirm = new EventEmitter();
